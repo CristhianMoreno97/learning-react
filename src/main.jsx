3c6749a032c4dd1a615068bc122abfb40c5bac65
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-10 07:55:23 -0500

    feat: add main.jsx component
    Added main.jsx component to the project, rendering a simple "Hello World" message using React.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+function App() {
+  return (<h1>Hello World</h1>)
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
