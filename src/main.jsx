97692bd637020ffc1745edc373bdd777ffd68040
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-10 08:31:30 -0500

    feat: Add styles.css file and update main.jsx to import it
    Added a new file, `styles.css`, containing CSS styles for the HTML and body elements. Updated `main.jsx` to import the `styles.css` file, applying the defined styles to the application.
    
    By separating the CSS styles into a dedicated file, the codebase becomes more organized and maintainable. Importing the CSS file into `main.jsx` ensures that the styles are applied globally across the application, providing consistent visual appearance. This demonstrates how to integrate CSS styles into a React application and manage global styles effectively.

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HelloWorld } from './HelloWorldApp'
 
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
