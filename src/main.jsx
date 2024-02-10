3925959b052b4af82becb8ce80985ae8bbe7191c
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-10 08:06:29 -0500

    feat: Modify main.jsx and add HelloWorldApp.jsx
    Updated main.jsx to import and render HelloWorld component from HelloWorldApp.jsx. Added new file HelloWorldApp.jsx containing the HelloWorld component.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HelloWorld } from './HelloWorldApp'
 
-function App() {
-  return (<h1>Hello World</h1>)
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelloWorld />
   </React.StrictMode>,
 )
