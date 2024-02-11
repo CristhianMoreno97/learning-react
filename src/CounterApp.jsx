4ebe25d67b81befa5bee1d80450e443768907f4e
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-11 12:42:53 -0500

    feat: Enhance CounterApp component with additional functionality
    Updated the Counter component in CounterApp.jsx to include additional functionality for decrementing the counter value and resetting it to its initial value. Added event handlers for decrementing the counter (`handleRemove`) and resetting it (`handleReset`), ensuring that the counter value remains within the specified range.
    
    The `handleRemove` function decrements the counter value by 1 only if it's greater than 0, preventing negative values. This ensures that the counter remains non-negative and behaves as expected.
    
    The `handleReset` function resets the counter value to its initial value, providing users with the ability to reset the counter to its original state.
    
    By adding these functionalities, we enhance the usability and versatility of the Counter component, allowing users to increment, decrement, and reset the counter value as needed.
    
    This modification demonstrates the iterative development process in React, where components are continuously refined and expanded to meet evolving requirements, enhancing the overall user experience.

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -4,8 +4,19 @@ import PropTypes from 'prop-types'
 export const Counter = ({ value }) => {
 
     const [counter, setCounter] = useState(value)
+
     const handleAdd = () => {
         setCounter(counter + 1)
+    }
+
+    const handleRemove = () => {
+        if (counter > 0) {
+            setCounter(counter -1)
+        }
+    }
+
+    const handleReset = () => {
+        setCounter(value)
     }
 
     return (
@@ -14,6 +25,8 @@ export const Counter = ({ value }) => {
             <p>{counter}</p>
 
             <button onClick={handleAdd}>+1</button>
+            <button onClick={handleRemove}>-1</button>
+            <button aria-label='btn-reset' onClick={handleReset}>Reset</button>
         </>
     )
 }
