0e32242cafb2b248083099e4ebcc8bedcd648be7
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-02-10 19:58:10 -0500

    chore(test): Set up testing environment for React components
    Installed necessary dependencies and configured the testing environment to enable testing of React components. Installed `@testing-library/react` for testing React components using Testing Library, `jest-environment-jsdom` for simulating the DOM environment within Jest tests, and `whatwg-fetch` for polyfilling the Fetch API.
    
    Added `@babel/preset-react` to the Babel configuration (`babel.config.cjs`) to enable Babel to transpile JSX syntax in React components.
    
    Created `jest.config.cjs` file to configure Jest, specifying the test environment as `jest-environment-jsdom` to use JSDOM as the test environment. Also, added `setupFiles` option to specify a setup file (`jest.setup.js`) to be executed before running the tests.
    
    In `jest.setup.js`, imported `whatwg-fetch` to polyfill the Fetch API in the testing environment, ensuring compatibility with components that make use of Fetch for data fetching.
    
    This setup enables the project to perform comprehensive testing of React components using Testing Library within Jest, enhancing the reliability and stability of the codebase. It also demonstrates the process of configuring Jest for testing React components, including installing dependencies, configuring Jest, and setting up Babel for transpilation.

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,4 +1,6 @@
 module.exports = {
     presets: [
-      ['@babel/preset-env', {targets: {node: 'current'}}]],
+      ['@babel/preset-env', {targets: {esmodules: 'current'}}],
+      ['@babel/preset-react', {runtime: 'automatic'}],
+    ],
   };
