c2733030a84451f5e287bc7f1e59bcc87fab9b87
CristhianMoreno97 <cristian_21_97@hotmail.com>
2024-03-31 13:21:57 -0500

    refactor: update ZoomableComponent
    - useCallback was added for the handleScroll function, which optimizes performance by avoiding the creation of a new function on each render.
    
    - Maximum and minimum values for the zoom level (maxZoomLevel and minZoomLevel) were defined, and it was ensured that the new zoom level was within these limits.
    - Zoom increment values, maximum and minimum zoom levels were extracted as constants outside of handleScroll to facilitate future adjustments and improve readability.

--- src/components/ZoomableComponent.jsx
+++ src/components/ZoomableComponent.jsx
@@ -1,30 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 const ZoomableComponent = () => {
   const [zoomLevel, setZoomLevel] = useState(1);
+  const zoomIncrement = 0.1;
+  const maxZoomLevel = 2; // Máximo nivel de zoom
+  const minZoomLevel = 0.5; // Mínimo nivel de zoom
+
+  const handleScroll = useCallback((event) => {
+    const deltaY = event.deltaY;
+
+    setZoomLevel(prevZoom => {
+      let newZoom = prevZoom + (deltaY < 0 ? zoomIncrement : -zoomIncrement);
+      newZoom = Math.min(Math.max(newZoom, minZoomLevel), maxZoomLevel); // Asegurar que el nuevo nivel de zoom esté entre los límites
+      return newZoom;
+    });
+  }, [zoomIncrement, minZoomLevel, maxZoomLevel]);
 
   useEffect(() => {
-    const handleScroll = (event) => {
-      const zoomIncrement = 0.1;
-      const deltaY = event.deltaY;
-
-      if (deltaY > 0) {
-        setZoomLevel((prevZoom) => prevZoom - zoomIncrement);
-      } else if (deltaY < 0) {
-        setZoomLevel((prevZoom) => prevZoom + zoomIncrement);
-      }
-    };
-
     window.addEventListener('wheel', handleScroll);
-
-    return () => {
-      window.removeEventListener('wheel', handleScroll);
-    };
-  }, []);
+    return () => window.removeEventListener('wheel', handleScroll);
+  }, [handleScroll]);
 
   return (
-    <div style={{ transform: `scale(${zoomLevel})`, transformOrigin: '0 0', border: '1px solid black', width: '300px', height: '200px', overflow: 'hidden' }}>
-      <img src="https://via.placeholder.com/300x200" alt="Zoomable Image" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+    <div style={{
+      transform: `scale(${zoomLevel})`,
+      transformOrigin: '0 0',
+      border: '1px solid black',
+      width: '300px',
+      height: '200px',
+      overflow: 'hidden'
+    }}>
+      <img
+        src="https://via.placeholder.com/300x200"
+        alt="Zoomable Image"
+        style={{ width: '100%', height: '100%', objectFit: 'cover' }}
+      />
     </div>
   );
 };
